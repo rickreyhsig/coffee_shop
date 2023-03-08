4944fc4ecbdb569fe8383d0e036e8d80e9cfbace
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2023-03-07 21:45:37 -0500

    Clean up code

--- app/services/combo_discount.rb
+++ app/services/combo_discount.rb
@@ -14,15 +14,15 @@ module Services
     def compute_order_total
       order_total = 0.0
       @order.order_items.each do |order_item|
-        item_description = order_item.item.description
-        if (@combo_discounts[item_description.to_sym])
-          discount = (1-@combo_discounts[item_description.to_sym])
-        else
-          discount = 1
-        end
+        discount = item_discount(order_item)
         order_total += (order_item.item.price * discount * order_item.quantity).to_f
       end
       return order_total
+    end
+
+    def item_discount(order_item)
+      item = order_item.item.description
+      return @combo_discounts[item.to_sym] ? (1-@combo_discounts[item.to_sym]) : 1
     end
   end
 end
