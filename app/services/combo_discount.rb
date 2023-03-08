422cea4f70cdc15b55774b1bda61f3932c3b3d86
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2023-03-07 21:37:42 -0500

    Minor improvements

--- app/services/combo_discount.rb
+++ app/services/combo_discount.rb
@@ -1,7 +1,7 @@
 module Services
   class ComboDiscount
     def initialize(order, combo_discounts)
-      @order = order
+      @order = Order.includes(order_items: :item).find(order.id)
       @combo_discounts = combo_discounts
       @order_total = nil
     end
@@ -15,14 +15,14 @@ module Services
 
     def compute_order_total
       order_total = 0.0
-      @order.includes(order_items: :item).each do |order_item|
+      @order.order_items.each do |order_item|
         item_description = order_item.item.description
         if (@combo_discounts[item_description.to_sym])
           discount = (1-@combo_discounts[item_description.to_sym])
         else
           discount = 1
         end
-        order_total += (order_item.item.price* discount * order_item.quantity).to_f
+        order_total += (order_item.item.price * discount * order_item.quantity).to_f
       end
       return order_total
     end
--- spec/services/combo_discount_spec.rb
+++ spec/services/combo_discount_spec.rb
@@ -17,41 +17,40 @@ describe Services::ComboDiscount do
       FactoryBot.create(:order_item, item: candy, quantity: 2, order: @order)
     end
 
-    describe
-      describe 'calculates order total for when there is no discount' do
-        let!(:combo_discounts) { {} }
+    describe 'calculates order total for when there is no discount' do
+      let!(:combo_discounts) { {} }
 
-        # 3.0*(1) + 5.0*(1) + 4.0*(2) = $16.0
-        it 'when there is no discount' do
-          expect(subject.process).to eq(16.0)
-        end
+      # 3.0*(1) + 5.0*(1) + 4.0*(2) = $16.0
+      it 'when there is no discount' do
+        expect(subject.process).to eq(16.0)
       end
+    end
 
-      describe 'calculates order total for when there is discount' do
-        let!(:combo_discounts) { { candy: 0.10 } }
+    describe 'calculates order total for when there is discount' do
+      let!(:combo_discounts) { { candy: 0.10 } }
 
-        # 3.0*(1) + 5.0*(1) + 4.0*(2)*(0.9) = $15.2
-        it 'for candy' do
-          expect(subject.process).to eq(15.2)
-        end
+      # 3.0*(1) + 5.0*(1) + 4.0*(2)*(0.9) = $15.2
+      it 'for candy' do
+        expect(subject.process).to eq(15.2)
       end
+    end
 
-      describe 'calculates order total for when there is combined discount' do
-        let!(:combo_discounts) { { beverage: 0.15, candy: 0.10 } }
+    describe 'calculates order total for when there is combined discount' do
+      let!(:combo_discounts) { { beverage: 0.15, candy: 0.10 } }
 
-        # 3.0*(1)*(0.85) + 5.0*(1) + 4.0*(2)*(0.9) = $14.75
-        it 'for beverage and candy' do
-          expect(subject.process).to eq(14.75)
-        end
+      # 3.0*(1)*(0.85) + 5.0*(1) + 4.0*(2)*(0.9) = $14.75
+      it 'for beverage and candy' do
+        expect(subject.process).to eq(14.75)
       end
+    end
 
-      describe 'calculates order total for when there is combined discount' do
-        let!(:combo_discounts) { { beverage: 0.15, sandwich: 0.20, candy: 0.10 } }
+    describe 'calculates order total for when there is combined discount' do
+      let!(:combo_discounts) { { beverage: 0.15, sandwich: 0.20, candy: 0.10 } }
 
-        # 3.0*(1)*(0.85) + 5.0*(1)*(0.8) + 4.0*(2)*(0.9) = $13.75
-        it 'for beverage, sandwich and candy' do
-          expect(subject.process).to eq(13.75)
-        end
+      # 3.0*(1)*(0.85) + 5.0*(1)*(0.8) + 4.0*(2)*(0.9) = $13.75
+      it 'for beverage, sandwich and candy' do
+        expect(subject.process).to eq(13.75)
       end
+    end
   end
 end
